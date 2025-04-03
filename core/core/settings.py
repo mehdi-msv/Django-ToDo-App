cb6c1898b3f5b1b76ad6330f935509891b33f4d7
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-04-03 17:10:54 +0330

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x91p8cw#p*u-e3uyz8r8n#^ns@g_(9o_&20zm#^t^u3u47-vp)'
+SECRET_KEY = config('SECRET_KEY',default='test')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool,default=True)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')],default='*')
 
 # Application definition
 
